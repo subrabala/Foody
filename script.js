c0fac92881a091d63496e84b6ddfb87267affe7d
subrabala <subrabala_dash@srmap.edu.in>
2023-02-05 19:48:13 +0530

    added api

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const searchBtn = document.getElementById('search-btn');
-const getRecipie = document.getElementById('get-recipie');
+const mealList = document.getElementById('meal-result');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 const mealDetailsContent = document.querySelector('.meal-details');
 
 // event listeners
 
 searchBtn.addEventListener('click', getMealList);
-getRecipie.addEventListener('click', getMealRecipie);
+mealList.addEventListener('click', getMealRecipe);
 
 recipeCloseBtn.addEventListener('click' , ()=>{
     mealDetailsContent.parentElement.classList.remove('showRecipie');
@@ -16,9 +16,9 @@ recipeCloseBtn.addEventListener('click' , ()=>{
 
 function getMealList(){
     let ingredient=document.getElementById('search-input').value.trim();
-    
+
     // adding search input to api's address
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
 
     //  FETCHING API
     .then(response => response.json())
@@ -27,21 +27,90 @@ function getMealList(){
         // if meal is available
         if(data.meals){
             // then for every meal
-            data.meals.forEach(meal => {
+            data.meals.forEach((meal, index) => {
                 // the html content will be changed
+                console.log(index);
                 html += `
-                <div class="meal-result">
-                <div id="meal">
-                   <img src="PotatoChips.jpeg" class="image" alt="food">
+                ${index %3 ===0 ? '<div class="meal-row">' : ''}
+                <div class="meal-cell">
+                <div class="meal" data-id="${meal.idMeal}"/>
+                   <img src="${meal.strMealThumb}" class="image" alt="food"/>
                  <div class="meal-name">
-                   <h3>Potato chips</h3>
-                   <a href="#" class="get-recipie" id="get-recipie">Get Recipie</a>
+                   <h3>${meal.strMeal}</h3>
+                   <a href="#" class="get-recipie" id="get-recipie">Get Recipe</a>
                  </div>
-               </div>
-             </div>
-                `
+                 </div>
+                </div>
+                ${index %3 ===2 || index===data.meals.length-1 ? '</div>' : ''}
+                `;
+
+                
             })
+
+            mealList.classList.remove('notFound');
         }
-    })
+        else{
+            html= "Sorry, no recipies found with this ingredient :(";
+            mealList.classList.add('notFound');
+        }
+
+        // mealList content = the dynamic content we just wrote
+        mealList.innerHTML = html;
+
+    });
+}
+
+
+
+
+// onclick of get recipie
+function getMealRecipe(e){
+    e.preventDefault()
+    // to prevent default actio of html page
+    if(e.target.classList.contains('recipie-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.id}`)
+        .then(response =>response.json())
+        .then(data => mealRecipeModal(data.meals));
+
+    }
+    console.log('clicked');
 
 }
+
+// the data to be displayed
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal=meal[0];
+    let html = `
+        <button class="close recipie-close" id="recipie-close">
+        <i class="fas fa-times btn-close"></i>
+        </button>
+        <div class="details-content">
+        <div class="detail-title">
+            ${meal.strMeal}
+        </div>
+        <div class="detail-category">
+            ${meal.strCategory}
+        </div>
+        <h3>Instructions :</h3>
+        <div class="detail-recipie">
+        ${meal.strInstructions}
+            </div>
+        <div class="detail-image">
+            <img src="${meal.strMealThumb}" class="detail-image" alt="">
+        </div>
+        <div class="link">
+            <a href="${meal.strYoutube}" class="link-el">Watch Video</a>
+        </div>
+        </div>
+    `;
+    mealDetailsContent.innerHTML =html;
+    // the outer block's content is now the one displayed in 'html' variable
+    mealDetailsContent.parentElement.classList.add('showRecipie');
+
+}
+
+
+
+
